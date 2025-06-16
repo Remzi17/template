64555cc294983db033e6660b6929c16ce4022682
Remzi17 <99osmanov98@gmail.com>
2025-06-16 21:44:07 +0300

    создал гит

---
+++ src/assets/js/scripts/components.js
@@ -0,0 +1,11 @@
+import { burger } from '../components/burger';
+import { popup } from '../components/popup';
+import { fixedMenu } from '../components/fixedmenu'
+import { gallery } from '../components/gallery'
+import { subMenu } from '../components/sub-menu'
+
+burger();
+popup();
+fixedMenu();
+gallery();
+subMenu();
