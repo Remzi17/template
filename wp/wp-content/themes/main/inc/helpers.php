3fc8b7d1a8342848d24b64405b8c4943cda232e3
Remzi17 <81004282+Remzi17@users.noreply.github.com>
2025-12-29 21:05:11 +0300

    Delete wp/wp-content directory

--- wp/wp-content/themes/main/inc/helpers.php
+++
@@ -1,6 +0,0 @@
-<? 
-
-function getPhone($field = 'телефон', $option = 'option', $sub_field = false) {
-	$value = $sub_field ? get_sub_field($field, $option) : get_field($field, $option);
-	return strip_tags(str_replace([' ', '(', ')', '-'], '', $value));
-}
--- wp/wp-content/themes/main/inc/other.php
+++
@@ -1,7 +0,0 @@
-<?
-	// Удаление тегов из excerpt
-	function clean_excerpt($excerpt) {
-		return strip_tags($excerpt);
-	}
-
-	add_filter('the_excerpt', 'clean_excerpt');
